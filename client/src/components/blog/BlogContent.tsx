d1cceee852af0fc5511b064d61fd4365fe50c86a
Walter Andrade <62224146+walteraandrade@users.noreply.github.com>
2026-08-24 18:46:25 -0300

    rm shiki (#22)

--- client/src/components/blog/BlogContent.tsx
+++ client/src/components/blog/BlogContent.tsx
@@ -1,50 +1,8 @@
-import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
-import { codeToHtml } from "shiki";
 
 interface BlogContentProps {
   content: string;
 }
-
-const CodeBlock = ({ className, children }: { className?: string; children?: React.ReactNode }) => {
-  const [html, setHtml] = useState<string>("");
-  const match = /language-(\w+)/.exec(className || "");
-  const lang = match ? match[1] : "text";
-  const code = String(children).replace(/\n$/, "");
-
-  useEffect(() => {
-    codeToHtml(code, {
-      lang,
-      theme: "github-dark",
-    }).then(setHtml);
-  }, [code, lang]);
-
-  if (!html) {
-    return (
-      <pre className="bg-black/90 border-brutal p-4 overflow-x-auto">
-        <code className="font-mono text-xs text-white">{code}</code>
-      </pre>
-    );
-  }
-
-  return (
-    <div className="relative group">
-      <div
-        className="[&>pre]:!bg-black/90 [&>pre]:border-brutal [&>pre]:p-4 [&>pre]:overflow-x-auto [&_code]:font-mono [&_code]:text-xs"
-        dangerouslySetInnerHTML={{ __html: html }}
-      />
-      <div className="absolute top-2 right-2 font-mono text-[10px] text-white/50 uppercase opacity-0 group-hover:opacity-100 transition-opacity">
-        {lang}
-      </div>
-    </div>
-  );
-};
-
-const InlineCode = ({ children }: { children?: React.ReactNode }) => (
-  <code className="font-mono text-xs bg-[#1d1c16] border border-[#3a382f] px-1.5 py-0.5 text-[var(--accent)]">
-    {children}
-  </code>
-);
 
 export function BlogContent({ content }: BlogContentProps) {
   return (
@@ -69,14 +27,6 @@ export function BlogContent({ content }: BlogContentProps) {
               </h3>
             );
           },
-          code: ({ className, children }) => {
-            const isBlock = className?.includes("language-");
-            if (isBlock) {
-              return <CodeBlock className={className}>{children}</CodeBlock>;
-            }
-            return <InlineCode>{children}</InlineCode>;
-          },
-          pre: ({ children }) => <>{children}</>,
         }}
       >
         {content}
